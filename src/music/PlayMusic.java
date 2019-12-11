a53b8d4c4fa7f921cf85cb6100ccf22e84c66f75
SuzerainLex <52357125+SuzerainLex@users.noreply.github.com>
2019-12-11 23:54:46 +0300

    Добалены mp3.

--- src/music/PlayMusic.java
+++ src/music/PlayMusic.java
@@ -1,27 +1,29 @@
 package music;
+
 import javazoom.jl.player.advanced.*;
 import javazoom.jl.player.*;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-public class PlayMusic extends Thread{
+public class PlayMusic extends Thread {
     public static AdvancedPlayer explay;
-    public static String muss1 = "Music\\warpigs.mp3";
-    public static String muss2 = "Music\\hand.mp3";
+    public static String track1 = "musicList\\warpigs.mp3";
+    public static String track2 = "musicList\\hand.mp3";
+
     @Override
     public void run() {
 
-        try{
-            InputStream potok = new FileInputStream(muss1);
+        try (InputStream potok = new FileInputStream(track1); InputStream potok1 = new FileInputStream(track2)) {
             AudioDevice auDev = new JavaSoundAudioDevice();
-            explay = new AdvancedPlayer(potok,auDev);
-            explay.play();
-            InputStream potok1 = new FileInputStream(muss2);
             AudioDevice auDev1 = new JavaSoundAudioDevice();
-            explay = new AdvancedPlayer(potok1,auDev1);
+            explay = new AdvancedPlayer(potok, auDev);
             explay.play();
-        }catch(Exception err){err.printStackTrace();}
+            explay = new AdvancedPlayer(potok1, auDev1);
+            explay.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
